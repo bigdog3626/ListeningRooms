d8233d33b3842b0fedec0eeac6a9000939f6fa24
bigdog3626 <cdm80383@uga.edu>
2022-05-04 00:23:33 -0400

    Mixing React and Django

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route paht="/room/:roomCode" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
