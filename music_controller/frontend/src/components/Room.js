fb7953b3a8c2485a5bdcd7a68364211960a35ab4
bigdog3626 <cdm80383@uga.edu>
2022-05-09 15:11:11 -0400

    Sessions Control Implemented

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class Room extends Component {
   constructor(props) {
@@ -11,6 +13,7 @@ export default class Room extends Component {
     this.roomCode = this.props.location.pathname.split("/")[2];
     this.getRoomDetails();
     console.log("the room code is", this.roomCode);
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
   }
   getRoomDetails() {
     fetch("/api/get-room" + "?code=" + this.roomCode)
@@ -25,15 +28,50 @@ export default class Room extends Component {
       });
   }
 
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+  }
+
   render() {
     console.log(this.roomCode);
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-        <p>Host: {this.state.isHost.toString()} </p>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Votes to Skip: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Is Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary">
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+
+      // <div>
+      //   <h3>{this.roomCode}</h3>
+      //   <p>Votes: {this.state.votesToSkip}</p>
+      //   <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+      //   <p>Host: {this.state.isHost.toString()} </p>
+      // </div>
     );
   }
 }
